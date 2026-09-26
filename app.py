81d942fd7476c050fbe3a8637f79f7432fc7ec4c
Fredaric Biju <fredaricbiju@gmail.com>
2026-09-26 05:32:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,16 +66,15 @@ def load_data():
         .str.strip()
     )
 
-   df['cost_escalation_pct'] = np.where(
+    df['cost_escalation_pct'] = np.where(
         (df['original_cost'] > 0) &
-        (df['revised_cost'] > 0)
-    ),
-    (
-        df['revised_cost'] /
-        df['original_cost'] - 1
-    ) * 100,
-    np.nan
-)
+        (df['revised_cost'] > 0),
+        (
+            df['revised_cost'] /
+            df['original_cost'] - 1
+        ) * 100,
+        np.nan
+    )
 
     df['expenditure_share_pct'] = np.where(
         df['revised_cost'] > 0,
